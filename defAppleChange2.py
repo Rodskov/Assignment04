68cc59b00858db483d61ead2583fabf58c5da7be
Justin Carl de Guia <justincarlcdeguia@iskolarngbayan.pup.edu.ph>
2021-11-06 11:25:03 +0800

    Second commit

--- defAppleChange2.py
+++ defAppleChange2.py
@@ -10,21 +10,17 @@ def askUser():
     return moneyNumber, applePrice
 
 #Step2 Calculate the maximum number of apples the money can buy
+#      and the change
 def maxApples(money_, applePrice_):
     maxNumber = money_//applePrice_
-    return maxNumber
+    appleChange = money_%applePrice_
+    return maxNumber, appleChange
 
-#Step3 Calculate the change
-def moneyChange(money_, applePrice_):
-    change = money_%applePrice_
-    return change
-
-#Step4 Let the user know the maximum number of apples the money can buy
+#Step3 Let the user know the maximum number of apples the money can buy
 #      and the change
 def userKnow():
     print(f"You can buy {maxNumber} apples and your change is {appleChange} pesos.")
 
 moneyNumber, applePrice = askUser()
-maxNumber = maxApples(moneyNumber, applePrice)
-appleChange = moneyChange(moneyNumber, applePrice)
+maxNumber, appleChange = maxApples(moneyNumber, applePrice)
 userKnow()
